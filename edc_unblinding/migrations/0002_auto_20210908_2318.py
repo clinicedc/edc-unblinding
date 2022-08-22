d5f6ef496daf720bacace3060c23480317bc1957
erikvw <ew2789@gmail.com>
2022-08-21 21:14:06 -0500

    fix path to django_audit_fields.admin

--- edc_unblinding/migrations/0002_auto_20210908_2318.py
+++ edc_unblinding/migrations/0002_auto_20210908_2318.py
@@ -7,28 +7,58 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('edc_unblinding', '0001_initial'),
+        ("edc_unblinding", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='historicalunblindingrequest',
-            name='requestor',
-            field=models.ForeignKey(blank=True, db_constraint=False, help_text='Choose a name from the list', null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='edc_unblinding.unblindingrequestoruser', verbose_name='Unblinding requested by'),
+            model_name="historicalunblindingrequest",
+            name="requestor",
+            field=models.ForeignKey(
+                blank=True,
+                db_constraint=False,
+                help_text="Choose a name from the list",
+                null=True,
+                on_delete=django.db.models.deletion.DO_NOTHING,
+                related_name="+",
+                to="edc_unblinding.unblindingrequestoruser",
+                verbose_name="Unblinding requested by",
+            ),
         ),
         migrations.AlterField(
-            model_name='historicalunblindingreview',
-            name='reviewer',
-            field=models.ForeignKey(blank=True, db_constraint=False, help_text='Choose a name from the list', null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='edc_unblinding.unblindingrevieweruser', verbose_name='Unblinding request reviewed by'),
+            model_name="historicalunblindingreview",
+            name="reviewer",
+            field=models.ForeignKey(
+                blank=True,
+                db_constraint=False,
+                help_text="Choose a name from the list",
+                null=True,
+                on_delete=django.db.models.deletion.DO_NOTHING,
+                related_name="+",
+                to="edc_unblinding.unblindingrevieweruser",
+                verbose_name="Unblinding request reviewed by",
+            ),
         ),
         migrations.AlterField(
-            model_name='unblindingrequest',
-            name='requestor',
-            field=models.ForeignKey(help_text='Choose a name from the list', on_delete=django.db.models.deletion.PROTECT, related_name='+', to='edc_unblinding.unblindingrequestoruser', verbose_name='Unblinding requested by'),
+            model_name="unblindingrequest",
+            name="requestor",
+            field=models.ForeignKey(
+                help_text="Choose a name from the list",
+                on_delete=django.db.models.deletion.PROTECT,
+                related_name="+",
+                to="edc_unblinding.unblindingrequestoruser",
+                verbose_name="Unblinding requested by",
+            ),
         ),
         migrations.AlterField(
-            model_name='unblindingreview',
-            name='reviewer',
-            field=models.ForeignKey(help_text='Choose a name from the list', on_delete=django.db.models.deletion.PROTECT, related_name='+', to='edc_unblinding.unblindingrevieweruser', verbose_name='Unblinding request reviewed by'),
+            model_name="unblindingreview",
+            name="reviewer",
+            field=models.ForeignKey(
+                help_text="Choose a name from the list",
+                on_delete=django.db.models.deletion.PROTECT,
+                related_name="+",
+                to="edc_unblinding.unblindingrevieweruser",
+                verbose_name="Unblinding request reviewed by",
+            ),
         ),
     ]
