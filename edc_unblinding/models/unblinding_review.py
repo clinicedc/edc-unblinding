1fadafce87b52d0cbc8fb0cd065cbbdc4fc90984
erikvw <ew2789@gmail.com>
2023-12-04 22:54:04 -0600

    review model meta class inheritance for indexes

--- edc_unblinding/models/unblinding_review.py
+++ edc_unblinding/models/unblinding_review.py
@@ -43,7 +43,7 @@ class UnblindingReview(
     def natural_key(self):
         return (self.action_identifier,)
 
-    class Meta:
+    class Meta(BaseUuidModel.Meta, NonUniqueSubjectIdentifierFieldMixin.Meta):
         verbose_name = "Unblinding Review"
         verbose_name_plural = "Unblinding Reviews"
         indexes = [
